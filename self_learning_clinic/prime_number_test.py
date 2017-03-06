5b876a70da0e0232b89e824ad6fb2e842f8a2a44
Flevian <fleviankanaiza@gmail.com>
2017-03-06 23:36:45 +0300

    modify prime_number_test.py file

--- self_learning_clinic/prime_number_test.py
+++ self_learning_clinic/prime_number_test.py
@@ -3,4 +3,7 @@ from prime_numbers import primenumbers
 
 class Test_prime_numbers(unittest.Testcase):
 	def test_returns_correct_primes(self):
-		self.assertTrue(primenumbers(20),[1,3,5,7,11,13,17,19]) 	
+		self.assertTrue(primenumbers(20), [1,3,5,7,11,13,17,19])
+
+	def test_assert_input_is_not_string(self):
+		self.assertRaises(TypeError, primenumbers('a_string'))		
