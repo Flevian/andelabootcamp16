8fb606aa3da8458b0c6f06a071d437d136660e81
Flevian <fleviankanaiza@gmail.com>
2017-03-07 10:42:57 +0300

    prime number test

--- self_learning_clinic/prime_number_test.py
+++ self_learning_clinic/prime_number_test.py
@@ -17,7 +17,11 @@ class Test_prime_numbers(unittest.TestCase):
 
 	def test_asserts_input_not_zero(self):
 		""" Input should be not be zero"""
-		self.assertIsNot(primesnumbers(0), 'Zero is not prime number')	
+		self.assertIsNot(primesnumbers(0), 'Zero is not prime number')
+		
+	def test_assert_input_is_not_string(self):
+		""" Input should be a an int"""
+		self.assertRaises(TypeError, primesnumbers('a_string'))		
 				    
 if __name__ == "__main__":
 	unittest.main()
