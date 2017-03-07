e219f29cad3e6f6d88593f45be4ac369cc0116b0
Flevian <fleviankanaiza@gmail.com>
2017-03-07 08:56:15 +0300

    the files to print prime numbers

--- self_learning_clinic/prime_number_test.py
+++ self_learning_clinic/prime_number_test.py
@@ -1,16 +1,28 @@
 import unittest
-from prime_numbers import primenumbers
+""" import function primesnumbers from prime_numbers file in order to test it"""
+from prime_numbers import primesnumbers
 
-class Test_prime_numbers(unittest.Testcase):
+class Test_prime_numbers(unittest.TestCase):
 	def test_returns_correct_primes(self):
-		self.assertTrue(primenumbers(20), [1,3,5,7,11,13,17,19])
+		""" Output should be prime numbers only"""
+		self.assertTrue(primesnumbers(25), [1, 3, 5, 7, 11, 13, 17, 19, 23])
 
 	def test_assert_input_is_not_string(self):
-		self.assertRaises(TypeError, primenumbers('a_string'))
+		""" Input should be a an int"""
+		self.assertRaises(TypeError, primesnumbers('a_string'))
 
 	def test_asserts_input_not_negative(self):
-		self.assertIs(primenumbers(-5), 'Please don\'t input negatives')
+		""" Input should be a positive value"""
+		self.assertIsNot(primesnumbers(-5), 'Please don\'t input negatives')
 
 	def test_returns_primes_in_array(self):
-		# Result should be an array
-		self.assertIsInstance(primenumbers(16), List)			
+		""" Result should be an array"""
+		self.assertIsInstance(primesnumbers(25), list)
+
+	def test_asserts_input_not_zero(self):
+		""" Input should be not be zero"""
+		self.assertIsNot(primesnumbers(0), 'Zero is not prime number')		
+	    
+if __name__ == "__main__":
+	unittest.main()
+
--- self_learning_clinic/prime_numbers.py
+++ self_learning_clinic/prime_numbers.py
@@ -1,2 +1,18 @@
-def primenumbers(prime_number_list):
-	pass
+def primesnumbers(number):
+    prime_number_list = list()
+
+    try:
+    	if number < 2:
+    		return False
+
+    	for num in range(number+1):
+    		"""Get the numbers that are only divisible by its self"""
+        	if all(num % i != 0 for i in range(2, int(num**.5 ) + 1)):
+        		if num < 2:
+        			pass
+        		else:
+        			prime_number_list.append(num)
+    	return (prime_number_list)  
+    except TypeError:
+    	"invalid input"	  
+  	      
