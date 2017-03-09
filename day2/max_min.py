7c50bac5cc57e26a37a67d997fee1f17dbd5a01a
Flevian <fleviankanaiza@gmail.com>
2017-03-09 13:42:03 +0300

    function for max and min numbers in a list

---
+++ day2/max_min.py
@@ -0,0 +1,37 @@
+def find_max_min(my_list):
+
+    """
+    function to get the minimum nd maximum values in a list and return the result in alist
+    """
+
+    min_max_result = []
+
+    if type(my_list) == list:
+
+        """
+        checks if the input is alist in order to perform the intended task
+        use max() and min() methods to find the maximum and minimum values from the list respectively and return the output in a list
+        """
+        max_value=max(my_list)
+        min_value=min(my_list)
+
+        if max_value == min_value:
+
+            """
+            checks if the maximum and minimum are equal
+            if they are equal
+            if they are not equal it returns a list that contains the minimum and maximum values
+            """
+            min_max_equals = len(my_list)
+            return    min_max_equals 
+
+        else:
+            min_max_result.append(min_value)
+            min_max_result.append(max_value)
+            return min_max_result
+
+    else:
+        return "invalid input"
+
+my_list = [67, 67]
+print(find_max_min(my_list)) 
