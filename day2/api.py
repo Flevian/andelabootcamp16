bdb8b13eb87c4407dcc4d2c5e24c6b89c99eafaf
Flevian <fleviankanaiza@gmail.com>
2017-03-09 14:15:41 +0300

    add comments

--- day2/api.py
+++ day2/api.py
@@ -1,13 +1,14 @@
 import requests
 import json
 def api(url):
+	"""
+	This function consumes an api for word's countries names and their capital cities 
+	"""
 	request_data = requests.get(url)
 	status_code = request_data.status_code
 	all_data = request_data.json()
-	  
 
 	return all_data
-
 
 url = "http://www.geognos.com/api/en/countries/info/all.json"
 print(api(url))
