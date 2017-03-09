1b24ec01b5c761bfccb30ee7889e41fc89970b4b
Flevian <fleviankanaiza@gmail.com>
2017-03-09 15:21:11 +0300

    modify the output appearance

--- day2/api.py
+++ day2/api.py
@@ -1,15 +1,16 @@
 import requests
 import json
+import pprint
 def api(url):
 	"""
 	This function consumes an api for word's countries names and their capital cities 
 	"""
 	request_data = requests.get(url)
 	status_code = request_data.status_code
-	all_data = request_data.json()
+	data = request_data.json()
 
-	return all_data
+	return data	
 
 url = "http://www.geognos.com/api/en/countries/info/all.json"
-print(api(url))
+pprint.pprint(api(url), width = 1)
 	
---
+++ day3/listcomprension.py
@@ -0,0 +1,11 @@
+class ListCompresnsion(object):
+	def __init__(list_length, list_step):
+		self.list_length == list_length
+
+		return self.list_length
+
+
+class  BinarySearch(desired_value):
+		count = {}
+
+		return count		
