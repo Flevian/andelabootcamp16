1d3f8ecd0b65fed8ce3afb282784a07908ad800b
Flevian <fleviankanaiza@gmail.com>
2017-03-09 18:30:40 +0300

    add a function that process a list to search desired value

--- day3/listcomprension.py
+++ day3/listcomprension.py
@@ -20,4 +20,39 @@ class ListCompresnsion(object):
 class  BinarySearch(ListCompresnsion):
 
 
-    pass
+    BinarySearch.__init__(self)
+    """
+    this class takes the list processed from the parent class to search for the desired value
+    """
+    def search(self, desired_value):
+      
+        """
+        this function takes in a list and the desired, searches the desired value and return the number of interation to get the desired value and the index of the value 
+        """
+
+        start = 0
+        last = len(self.my_list) - 1
+        count = 0
+        out_dict = {}
+
+        while start <= last:
+            mid_point = start + last // 2
+            count += 1
+
+            if self.my_list[mid_point] == desire_value:
+                """
+                compares the mid point value and the desired value
+                  """
+                index = [mid_point]
+                break
+
+
+            else:
+                if desire_value < self.my_list[mid_point]:
+                    last = mid_point - 1
+
+        out_dict[count] = index
+        return out_dict 
+
+
+
